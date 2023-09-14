72b9fc6094347588a260a7669fb06c624fb483b3
Hailie417 <192151452@qq.com>
2023-09-14 23:36:43 +0800

    新增页面

--- CMS-CoderWhy/src/main.ts
+++ CMS-CoderWhy/src/main.ts
@@ -4,5 +4,6 @@ import './assets/css/index.less'
 import router from './router'
 import App from './App.vue'
 
-createApp(App).use(router)
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
--- CMS-CoderWhy/src/router/index.ts
+++ CMS-CoderWhy/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHashHistory(),
+  //映射关系： path => components
   routes: []
 })
 export default router
