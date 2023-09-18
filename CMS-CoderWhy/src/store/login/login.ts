38d8ae5d753caaba5b02ffe6c3be114e93d63703
Hailie417 <192151452@qq.com>
2023-09-19 00:20:45 +0800

    登录跳转页面

--- CMS-CoderWhy/src/store/login/login.ts
+++ CMS-CoderWhy/src/store/login/login.ts
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia'
 import { accountLoginRequest } from '@/service/login/login'
 import type { IAcount } from '@/types'
+import router from '@/router'
+const LOGIN_TOKEN = 'login/token'
 
 const userLoginStore = defineStore('login', {
   state: () => ({
     id: '',
-    token: localStorage.getItem('token') ?? '',
+    token: localStorage.getItem(LOGIN_TOKEN) ?? '',
     name: ''
   }),
   actions: {
@@ -16,8 +18,11 @@ const userLoginStore = defineStore('login', {
       this.token = loginResult.data.token
       this.name = loginResult.data.name
 
-      // 2。 进行本地缓存
-      localStorage.setItem('token', this.token)
+      // 2. 进行本地缓存
+      localStorage.setItem(LOGIN_TOKEN, this.token)
+
+      // 3. 页面跳转 (main页面)
+      router.push('/main')
     }
   }
 })
