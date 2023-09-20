d8a6d635ef9b20f0a9240844420ebf486437a0df
Hailie <wang_junnan@hotmail.com>
2023-09-20 14:30:48 +0800

    261 header布局

--- CMS-CoderWhy/src/components/main-header/main-header.vue
+++ CMS-CoderWhy/src/components/main-header/main-header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="main-header">
-    <h2>main-header</h2>
+    <div class="menu-icon">
+      <el-icon size="28px"><Fold /></el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrunb">面包蟹</div>
+      <div class="info">个人信息</div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +14,21 @@
 
 <style lang="less" scoped>
 .main-header {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
 }
 </style>
