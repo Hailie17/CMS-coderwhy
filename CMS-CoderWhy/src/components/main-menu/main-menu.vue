4b2cb72dbb9fea5cecd18a1a7add818c72ae4873
Hailie <wang_junnan@hotmail.com>
2023-09-20 11:49:16 +0800

    左侧菜单栏布局搭建

---
+++ CMS-CoderWhy/src/components/main-menu/main-menu.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="main-menu">
+    <!-- 1. logo -->
+    <div class="logo">
+      <img class="img" src="@/assets/img/logo.svg" alt="" />
+      <h2 class="title">藏龙岛管理系统</h2>
+    </div>
+    <!-- 2. menu -->
+    <div class="menu">
+      <el-menu text-color="#b7bdc3" active-text-color="#fff" background-color="#001529">
+        <!-- 1. 系统总览 -->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+        <!-- 1. 系统管理-->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><Setting /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+        <!-- 1. 商品中心 -->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><Handbag /></el-icon>
+            <span>商品中心</span>
+          </template>
+          <el-menu-item>商品类别</el-menu-item>
+          <el-menu-item>商品信息</el-menu-item>
+        </el-sub-menu>
+        <!-- 4. 聊天 -->
+        <el-sub-menu>
+          <template #title>
+            <el-icon><ChatLineRound /></el-icon>
+            <span>随便聊聊</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.main-menu {
+  height: 100%;
+  background-color: #001529;
+}
+.logo {
+  display: flex;
+  height: 28px;
+  padding: 12px 10px 8px 10px;
+  flex-direction: row;
+  justify-self: flex-start;
+  align-items: center;
+  overflow: hidden;
+  .img {
+    height: 100%;
+    margin: 0 10px;
+  }
+  .title {
+    font-size: 16px;
+    font-weight: 600;
+    color: white;
+    white-space: nowrap;
+  }
+}
+
+.el-menu {
+  border-right: none;
+  user-select: none;
+}
+.el-sub-menu {
+  .el-menu-item {
+    padding-left: 50px !important;
+    background-color: #0c2133;
+  }
+  .el-menu-item:hover {
+    color: #fff;
+  }
+  .el-menu-item.is-active {
+    background-color: #0a60bd !important;
+  }
+}
+</style>
--- CMS-CoderWhy/src/views/main/main.vue
+++ CMS-CoderWhy/src/views/main/main.vue
@@ -1,19 +1,50 @@
 <template>
   <div class="mian">
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
+    <el-container class="main-content">
+      <el-aside width="210px">
+        <main-menu />
+      </el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header height="50px">
+          <main-header />
+        </el-header>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainMenu from '@/components/main-menu/main-menu.vue'
+import MainHeader from '@/components/main-header/main-header.vue'
+</script>
 
 <style lang="less" scoped>
 .mian {
-  color: pink;
+  width: 100%;
+  height: 100%;
+  .main-content {
+    height: 100%;
+    .el-aside {
+      overflow-x: hidden;
+      overflow-y: auto;
+      line-height: 200px;
+      text-align: left;
+      cursor: pointer;
+      background-color: #001529;
+      scrollbar-width: none; /*firefox*/
+      -ms-overflow-style: none; /*IE 10+*/
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+    .el-header {
+      background: purple;
+    }
+    .el-main {
+      background: #f0f2f5;
+    }
+  }
 }
 </style>
