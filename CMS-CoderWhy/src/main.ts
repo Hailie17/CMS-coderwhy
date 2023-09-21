83a2299bf79e74a0a5912b4be1bd2b14d23f7dd3
Hailie <192151452@qq.com>
2023-09-21 10:51:40 +0800

    把获取本地数据的操作放到store里面

--- CMS-CoderWhy/src/main.ts
+++ CMS-CoderWhy/src/main.ts
@@ -2,20 +2,17 @@ import { createApp } from 'vue'
 import 'normalize.css'
 import './assets/css/index.less'
 import router from './router'
+import store from './store'
 import App from './App.vue'
-import pinia from './store'
 import registerIcons from './global/register-icons'
 // 全局引入elementplus
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 // app.use(ElementPlus)
 import 'element-plus/theme-chalk/el-message.css'
-import userLoginStore from './store/login/login'
 
 const app = createApp(App)
 app.use(registerIcons)
-app.use(pinia)
-const loginStore = userLoginStore()
-loginStore.loadLocalCacheAction()
+app.use(store)
 app.use(router)
 app.mount('#app')
--- CMS-CoderWhy/src/store/index.ts
+++ CMS-CoderWhy/src/store/index.ts
@@ -1,4 +1,14 @@
 import { createPinia } from 'pinia'
+import type { App } from 'vue'
+import userLoginStore from './login/login'
 const pinia = createPinia()
 
-export default pinia
+function registerStore(app: App<Element>) {
+  // 1. use pinia
+  app.use(pinia)
+  //   2.加载本地数据
+  const loginStore = userLoginStore()
+  loginStore.loadLocalCacheAction()
+}
+
+export default registerStore
