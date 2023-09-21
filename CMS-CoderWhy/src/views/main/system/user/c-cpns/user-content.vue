3fe5b5b29b8337942832642b9c5efe05824cbd0c
Hailie417 <192151452@qq.com>
2023-09-21 23:25:44 +0800

    table

--- CMS-CoderWhy/src/views/main/system/user/c-cpns/user-content.vue
+++ CMS-CoderWhy/src/views/main/system/user/c-cpns/user-content.vue
@@ -4,12 +4,10 @@
       <h3 class="title">用户列表</h3>
       <el-button type="primary" size="large">新建用户</el-button>
     </div>
-    <div class="content">
-      <ul>
-        <li v-for="item in userList" :key="item.id">
-          {{ item.name }}
-        </li>
-      </ul>
+    <div class="table">
+      <el-table :data="userList" stripe style="width: 100%">
+        <el-table-column prop="name" label="用户名" width="180" />
+      </el-table>
     </div>
     <div class="pagination"></div>
   </div>
