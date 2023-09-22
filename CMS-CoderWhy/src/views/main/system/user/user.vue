d7a328cc2c2508064d88948349354a7ff9d678aa
Hailie <192151452@qq.com>
2023-09-22 16:57:02 +0800

    290 点击添加-弹出对话框

--- CMS-CoderWhy/src/views/main/system/user/user.vue
+++ CMS-CoderWhy/src/views/main/system/user/user.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="user">
     <user-search @query-click="handleQueryClick" @reset-click="handleResetClick" />
-    <user-content ref="contentRef" />
+    <user-content ref="contentRef" @click="handleAddBtnClick" />
+    <user-modal ref="modalRef" />
   </div>
 </template>
 
 <script setup lang="ts">
 import UserSearch from './c-cpns/user-search.vue'
 import UserContent from './c-cpns/user-content.vue'
+import UserModal from './c-cpns/user-modal.vue'
 import { ref } from 'vue'
 // 对content组件的操作
 const contentRef = ref<InstanceType<typeof UserContent>>()
@@ -16,6 +18,11 @@ function handleQueryClick(formData: any) {
 }
 function handleResetClick() {
   contentRef.value?.fetchUserListData()
+}
+// 对modal组件的操作
+const modalRef = ref<InstanceType<typeof UserModal>>()
+function handleAddBtnClick() {
+  modalRef.value?.dialogShow()
 }
 </script>
 
